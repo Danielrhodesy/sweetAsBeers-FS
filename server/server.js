36c9ff58a81888432047e649e52a38f2b6f4e1e3
Daniel Rhodes <danielrhodesnz@gmail.com>
2019-07-10 12:08:27 +1200

    static acess

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
-server.unsubscribe(express.static('public'))
+server.use(express.static('public'))
 
 module.exports = server
